999f06b0e82581f25b30a89c9c2ff7cc482a9ca2
JosXa <joscha.goetzer@gmail.com>
2018-05-06 18:42:02 +0200

    Fix wrong import

--- tgintegration/__init__.py
+++ tgintegration/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from tgintegration.integrationtestclient import BotIntegrationClient
+from tgintegration.botintegrationclient import BotIntegrationClient
 from tgintegration.interactionclient import InteractionClient
 from tgintegration.response import Response
 from tgintegration.awaitableaction import AwaitableAction
