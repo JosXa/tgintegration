0f6e5ccc91e99e9919e38db1f9fe78bed8adac2f
JosXa <joscha.goetzer@gmail.com>
2018-05-08 22:02:42 +0200

    go

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ import tgintegration
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxpyistubs']
+extensions = ['sphinx.ext.autodoc', 'sphinx_autodoc_typehints', 'sphinx.ext.viewcode', 'sphinxpyistubs']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
