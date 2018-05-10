1c3e31cbe6960bf835e63782bef87184b70343ba
JosXa <joscha.goetzer@gmail.com>
2018-05-11 00:31:01 +0200

    Bump version: 0.1.5 → 0.2.0

--- tgintegration/__init__.py
+++ tgintegration/__init__.py
@@ -8,7 +8,7 @@ from tgintegration.containers import InlineResult, InlineResultContainer
 
 __author__ = """Joscha Götzer"""
 __email__ = 'joscha.goetzer@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 __all__ = [
     "AwaitableAction", "Response", "InteractionClient", "BotIntegrationClient", "InlineResult",
