953057c7d24ad88ddd43858d39c7ca3f73a52890
BladzheR <BladzheR@users.noreply.github.com>
2018-10-25 23:15:45 +0300

    Update responce.py
    Error: "from pyrogram.client.types.reply_markup.reply_keyboard_markup import ReplyKeyboardMarkup
    E   ModuleNotFoundError: No module named 'pyrogram.client.types.reply_markup'"
    Answer:
    "try:
        from pyrogram.client.types.bots.reply_keyboard_markup import ReplyKeyboardMarkup
    except ImportError:
        from pyrogram.client.types.reply_markup.reply_keyboard_markup import ReplyKeyboardMarkup"

--- tgintegration/response.py
+++ tgintegration/response.py
@@ -2,7 +2,10 @@ import time
 from datetime import datetime
 
 from pyrogram import InlineKeyboardMarkup
-from pyrogram.client.types.reply_markup.reply_keyboard_markup import ReplyKeyboardMarkup
+try:
+    from pyrogram.client.types.bots.reply_keyboard_markup import ReplyKeyboardMarkup
+except ImportError:
+    from pyrogram.client.types.reply_markup.reply_keyboard_markup import ReplyKeyboardMarkup
 
 from tgintegration.containers import InlineKeyboard, ReplyKeyboard
 
